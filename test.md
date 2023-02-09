23bf2553c3d5c82450d50fa6f190dc110d81d138
Gage Norton <nortongage0@gmail.com>
2023-02-09 12:00:00 -0600

    Add write file and render badge markdown function

---
+++ test.md
@@ -0,0 +1,36 @@
+
+  # f
+
+  undefined
+
+  # Table of Contents
+  1. [application description](#description)
+  2. [Installation](#installation)
+  3. [Usage](#usage)
+  4. [License](#license)
+  5. [Contributing](#contributing)
+  6. [Tests](#test)
+  7. [Questions](#questions)
+
+  # Description
+  ## f
+
+  # Installation
+  ## f
+
+  # License 
+  ## f
+
+  # Usage
+  ## f
+  
+  # Contributing 
+  ## f
+
+  # Tests
+  ## undefined
+
+  # Questions 
+  ## f
+  ## "https://github.com/f"
+  ## f
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,8 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) { }
+function renderLicenseBadge(license) { 
+  console.log("i got it ");
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -12,8 +14,12 @@ function renderLicenseSection(license) { }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  const { license } = data
+  const badge = renderLicenseBadge(license)
   return `
   # ${data.title}
+
+  ${badge}
 
   # Table of Contents
   1. [application description](#description)
